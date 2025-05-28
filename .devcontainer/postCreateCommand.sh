1fffa5b778ab38716f6a1f262983637d8d3663cc
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-05-28 16:42:15 +0000

    Initial commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo chsh -s $(which zsh) $(whoami)
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install sqlite3 neovim -y
+sudo apt install libffi-dev libyaml-dev -y # For installing ruby versions with rbenv
+
+echo -e "\033[0;32mTo install rbenv, run:"
+echo -e "curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash"
+echo -e "Then, close and reopen both vscode, and the vscode embedded terminal\033[0m"
