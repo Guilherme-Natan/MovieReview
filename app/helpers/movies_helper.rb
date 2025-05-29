aec759731ff08f3e6f94f4f1c7b003d490f9e196
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-05-29 21:57:59 +0000

    Add a basic rendering  for the movies model

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,15 @@
 module MoviesHelper
+  def minutes_to_hours(minutes)
+    hours = minutes / 60
+    minutes %= 60
+    return "#{minutes} #{plural_or_singular("minute", minutes)}" if hours == 0
+    return "#{hours} #{plural_or_singular("hour", hours)}" if minutes == 0
+    "#{hours} #{plural_or_singular("hour", hours)} and #{minutes} #{plural_or_singular("minute", minutes)}"
+  end
+
+  private
+
+  def plural_or_singular(word, number)
+    number == 1 ? word : "#{word}s"
+  end
 end
