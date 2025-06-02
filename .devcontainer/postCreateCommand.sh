6b6fd06cbb05530ca1a5f44141ead7538689a1bb
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-06-02 23:03:34 +0000

    Add sorbet

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,6 +5,7 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install sqlite3 neovim -y
 sudo apt install libffi-dev libyaml-dev -y # For installing ruby versions with rbenv
+sudo apt install watchman -y # For installing ruby versions with rbenv
 
 echo -e "\033[0;32mTo install rbenv, run:"
 echo -e "curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash"
