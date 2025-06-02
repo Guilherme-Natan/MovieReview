72b09831bae54324bd21418b486a57bcbe323376
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-06-02 01:07:26 +0000

    Add process to update movies through the browser

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,17 @@ class MoviesController < ApplicationController
   def show
       @movie = Movie.find(params[:id])
   end
+
+  def edit
+      @movie = Movie.find(params[:id])
+  end
+
+  def update
+      @movie = Movie.find(params[:id])
+      movie_updated = params
+        .require(:movie)
+        .permit("title", "rating", "released_on", "total_gross", "description")
+      @movie.update(movie_updated)
+      redirect_to @movie
+  end
 end
