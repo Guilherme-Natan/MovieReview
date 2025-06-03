ee6d77c665d1d7014626af966d3881b0516f64ef
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-06-03 20:48:51 +0000

    Add delete movie functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,36 +1,42 @@
 class MoviesController < ApplicationController
   def index
-      @movies = Movie.all
+    @movies = Movie.all
   end
 
   def show
-      @movie = Movie.find(params[:id])
+    @movie = Movie.find(params[:id])
   end
 
   def edit
-      @movie = Movie.find(params[:id])
+    @movie = Movie.find(params[:id])
   end
 
   def update
-      @movie = Movie.find(params[:id])
-      @movie.update(movie_params)
-      redirect_to @movie
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to @movie
   end
 
   def new
-      @movie = Movie.new
+    @movie = Movie.new
   end
 
   def create
-      @movie = Movie.create(movie_params)
-      redirect_to @movie
+    @movie = Movie.create(movie_params)
+    redirect_to @movie
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_url, status: :see_other
   end
 
   private
 
   def movie_params
     params
-      .require(:movie)
-      .permit("title", "rating", "released_on", "total_gross", "description")
+     .require(:movie)
+     .permit("title", "rating", "released_on", "total_gross", "description")
   end
 end
