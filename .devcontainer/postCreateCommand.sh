b14f392e9930763b242762aac8825058f37df811
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-06-04 21:39:40 +0000

    Add lnav to devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,8 +3,10 @@
 sudo chsh -s $(which zsh) $(whoami)
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install sqlite3 neovim -y
+sudo apt install sqlite3 -y
+sudo apt install neovim -y # Text editor
 sudo apt install libffi-dev libyaml-dev -y # For installing ruby versions with rbenv
 sudo apt install watchman -y # For installing ruby versions with rbenv
+sudo apt install lnav -y # Interesting for reading logs
 
 echo -e "\033[0;32mRead ./devcontainer/README.md on how to setup this devcontainer\033[0m"
