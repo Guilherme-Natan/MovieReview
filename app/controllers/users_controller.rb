120f12f5ba90420abfd362b29b331e6b5087f1e4
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-07-18 15:55:40 +0000

    Add autologin on sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to @user, notice: "User successfully created!"
+      session[:user_id] = @user.id
     else
       render :new, status: :unprocessable_entity
     end
